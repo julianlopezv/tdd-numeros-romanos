e62f6d6a8b5d702eb43987ed77bdaec4ff3ab5a9
Julián Andrés López Valencia <julian@tupagina.com.co>
2016-05-06 22:42:25 -0500

    The algorithm convert One to Ten (refactoring)

--- roman.go
+++ roman.go
@@ -2,23 +2,18 @@ package roman
 
 func RomanToDecimal(roman string) int {
     count := 0
-	flag := 0
-	for i := 0; i < len(roman); i++ {
-		if roman[i] == 'I' {
-			count += 1
-			flag = 1
+	romans := len(roman)
+	for i := 0; i < romans; i++ {
+		if roman[i] == 'I'{
+			if i < romans - 1 && roman[i+1] != 'I'{
+				count -= 1
+			} else {
+				count += 1
+			}
 		} else if roman[i] == 'V' {
 			count += 5
-			if flag == 1 {
-				count -= 2
-				flag = 0
-			}	
 		} else if roman[i] == 'X' {
-			count += 10
-			if flag == 1 {
-				count -= 2
-				flag = 0
-			}	
+			count += 10	
 		}
     }
 	return count
