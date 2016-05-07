16395fab5bf0f6416d6edfb23c3b09abd498319a
Julián Andrés López Valencia <julian@tupagina.com.co>
2016-05-06 23:22:03 -0500

    The algorithm convert 49 (refactoring)

--- roman.go
+++ roman.go
@@ -2,24 +2,21 @@ package roman
 
 func RomanToDecimal(roman string) int {
     count := 0
+	lastval := 0
 	romans := len(roman)
 	for i := 0; i < romans; i++ {
 		if roman[i] == 'I'{
-			if i < romans - 1 && roman[i+1] != 'I'{
-				count -= 1
-			} else {
-				count += 1
-			}
+			count += 1
+			lastval = 2
 		} else if roman[i] == 'V' {
-			count += 5
+			count += 5 - lastval
+			lastval = 0
 		} else if roman[i] == 'X' {
-			if i < romans - 1 && roman[i+1] == 'L'{
-				count -= 10
-			} else {
-				count += 10
-			}	
+			count += 10
+			lastval = 20
 		} else if roman[i] == 'L' {
-			count += 50
+			count += 50 - lastval
+			lastval = 0
 		}
     }
 	return count
